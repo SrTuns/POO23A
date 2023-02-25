0f61bfc2790d910e3e1715c697e3de2f89238297
SrTuns <alexismontesescorcia@gmail.com>
2023-02-25 13:30:46 -0600

    a

--- POO23A_MA/Practicas/DlgConfigurar.cs
+++ POO23A_MA/Practicas/DlgConfigurar.cs
@@ -20,37 +20,28 @@ namespace POO23A_MA
 
         private void BtnGuardar_Click(object sender, EventArgs e)
         {
-            //StreamWriter outputFile;
+            StreamWriter outputFile;
 
-            //outputFile = new StreamWriter("C:\\Users\\PC\\Documents\\Universidad\\3er Semestre\\Programación Orientada a Objetos\\Descripciones\\" + CbxEscogerPractica.Text + ".txt");
+            outputFile = new StreamWriter("C:\\Users\\PC\\Documents\\Universidad\\3er Semestre\\Programación Orientada a Objetos\\Descripciones\\" + CbxEscogerPractica.Text + ".txt");
 
-            //string[] lineas = {
-            //    TbxDescripcion.Text};
+            string[] lineas = {
+                TbxDescripcion.Text};
 
-            //foreach (string linea in lineas)
-            //{
-            //    outputFile.WriteLine(linea);
-            //}
+            foreach (string linea in lineas)
+            {
+                outputFile.WriteLine(linea);
+            }
 
-            //outputFile.Close();
+            outputFile.Close();
 
-            //MessageBox.Show("Descripción guardada.");
+            MessageBox.Show("Descripción guardada.");
 
-            //TbxDescripcion.Text = "";
+            TbxDescripcion.Text = "";
 
-            //string fileName = Properties.Resources.Desc1;
-            //string text = TbxDescripcion.Text;
-
-            //using (StreamWriter writer = new StreamWriter(fileName))
-            //{
-            //    writer.Write(text);
-            //}
-
-            string fileName = "Desc1.txt"; // aquí debes proporcionar el nombre de archivo deseado
+            string fileName = Properties.Resources.Desc1;
             string text = TbxDescripcion.Text;
 
-            using (Stream stream = (Stream)Properties.Resources.ResourceManager.GetObject("Desc1"))
-            using (StreamWriter writer = new StreamWriter(stream))
+            using (StreamWriter writer = new StreamWriter(fileName))
             {
                 writer.Write(text);
             }
